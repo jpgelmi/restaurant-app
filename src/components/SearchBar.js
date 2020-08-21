bbd8a3c0558030247c0f0186b706e5459b5d8d58
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-08-21 13:15:10 -0400

    commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
+import {Icon} from "react-native-elements"
+import { color } from 'react-native-reanimated'
 
-export default function SearchBar() {
+export default function SearchBar(props) {
+    const{term, onTermChange, onTermSubmit} = props
     return (
         <View style = {styles.background}>
-            <Text>SearchBar</Text>
+            <Icon
+                name = "search"
+                size = {30}
+                marginHorizontal = {5}
+            />
+            <TextInput
+                placeholder = "Buscar..."
+                style = {styles.inputStyle}
+                value = {term}
+                autoCapitalize = "none"
+                autoCorrect = {false}
+                onEndEditing = {() => onTermSubmit()}
+                onChangeText = {newTerm => onTermChange(newTerm)}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     background:{
-        backgroundColor: "#F0EEEE",
+        backgroundColor: "#D3D3D3",
         height: 50,
-        borderRadius: 12,
-        marginHorizontal: 15
-    }
+        borderRadius: 10,
+        marginHorizontal: 15,
+        marginVertical: 10,
+        flexDirection : "row",
+        alignItems: "center",
+    },
+    inputStyle:{
+        flex: 1
+    },
 })
--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import SearchBar from "../components/SearchBar"
 
 export default function HomeScreen() {
+    const[term, setTerm] = useState("")
+
     return (
         <SafeAreaView>
             <View style = {styles.container}>
-                <SearchBar/>
+                <SearchBar
+                term = {term}
+                onTermChange = {newTerm => setTerm(newTerm)}
+                onTermSubmit = {() => console.log("El termino fue enviado")}
+                />
+                <Text>{term}</Text>
             </View>
         </SafeAreaView>
     )
@@ -16,3 +23,4 @@ const styles = StyleSheet.create({
     container:{
     }
 })
+ 
