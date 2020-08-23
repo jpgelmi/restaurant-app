3cfa67aaefbcb0cad4ed5d7dfbf568297b0d868c
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-08-22 23:23:50 -0400

    commit

--- src/components/ResultDetails.js
+++ src/components/ResultDetails.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
 export default function ResultDetails({result}) {
     return (
         <View>
-            <Text>{result.name}</Text>
+            <Image
+                source = {{uri: result.image_url}}
+                style = {styles.imageStyle}/>
+            <Text style = {styles.nameStyle}>{result.name}</Text>
+            <Text>{result.rating} Calificación, {result.review_count} Reviews</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imageStyle:{
+        width: 250,
+        height: 120,
+        borderRadius: 12,
+    },
+    nameStyle:{
+        fontWeight: "bold",
+        fontSize: 15
+    }
+})
